fd0b1dc069b23431ab86d42b4f61c5651744b176
Danielle Davis <danidavis321@gmail.com>
2021-11-28 17:31:45 -0600

    function, variable, and console practice

---
+++ assets/js/game.js
@@ -0,0 +1,18 @@
+// this creates a function called "fight"
+
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt ("What is your robot's name?")
+
+console.log(playerName);
+
+console.log("This logs a sting, good for leaving yourself a message")
+//this will do math and log 20
+console.log(10+10)
+//what is this? - This will put the name that was inputted earlier at the end of the sentance. :) 
+console.log("Our Robot's name is" + playerName);
+
+
+// fight ();
