ed57bef277f958a44aea6cc02d304a5d5698fcc2
Danielle Davis <danidavis321@gmail.com>
2021-11-29 18:16:27 -0600

    add for loop to fight each robot in the array

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,22 +12,12 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// feedback to appear in the console
-console.log(playerName, playerAttack, playerHealth);
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
-    //display enemyNames array in the console
-    console.log(enemyNames);
-    console.log(enemyNames[0]);
-    console.log(enemyNames[1]);
-    console.log(enemyNames[2]);
-    console.log(enemyNames[3]);
-
 var enemyHealth = 50;
 var enemyAttack = 12;
 
 // fight function
-var fight = function() {
+var fight = function(enemyName) {
   // Alert players that they are starting the round
   window.alert("Welcome to Robot Gladiators!");
 
@@ -82,6 +72,6 @@ var fight = function() {
   }
 };
 
-
-// run fight function to start game
-// fight();
+for(var i=0; i < enemyNames.length; i++) {
+    fight (enemyNames[i]);
+}
