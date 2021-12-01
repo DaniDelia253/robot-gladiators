0867332c2c66a300565b6703843d09a6566e3649
Danielle Davis <danidavis321@gmail.com>
2021-11-30 20:44:42 -0600

    added multiple rounds with round number alerts

--- assets/js/game.js
+++ assets/js/game.js
@@ -65,7 +65,19 @@ var fight = function(enemyName) {
     }
 }
 for(var i=0; i < enemyNames.length; i++) {
-    var pickedEnemyName = enemyNames[i];
-    enemyHealth = 50;
-    fight (enemyNames[i]);
+    if (playerHealth > 0) {
+        //let the player know what round they are in
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1 ) );
+        //pick new enemy to fight based on the next enemy in the enemyNames array
+        var pickedEnemyName = enemyNames[i];
+        //reset enemyHealth before starting new fight
+        enemyHealth = 50;
+        //use debugger to pause script from running and check what's going on at that moment in the code
+        debugger;
+        // pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
+        fight (pickedEnemyName);
+    }
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+    }
 }
